252794fa8580a3fd25791f435efb4197852396a8
damianrivas <drivas1993@gmail.com>
2018-05-03 17:53:43 -0700

    Change title. Adjust content padding

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Scroll = (props) => {
   return (
-    <div style={{flex: '1', overflowY: 'scroll', border: '1px solid black'}}>
+    <section className="scroll-container" style={{flex: '1', overflowY: 'scroll', border: '1px solid black', padding: "25px 10px"}}>
       {props.children}
-    </div>
+    </section>
   )
 }
 
